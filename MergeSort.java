227e3f358e8268a8c4ca8b092d09ac39772bb654
rryaboshtan <rrvworkroman@gmail.com>
2021-12-02 01:46:52 +0200

    merge func

---
+++ MergeSort.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int dest[], int destStart, int size) {
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
+    }
+}

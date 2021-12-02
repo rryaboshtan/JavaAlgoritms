f0dfddd1293b83e665eaac4c8747c07fb0553bbd
rryaboshtan <rrvworkroman@gmail.com>
2021-12-02 02:13:18 +0200

    Change class name

--- MergeSortIterative.java
+++ MergeSortIterative.java
@@ -1,4 +1,4 @@
-public class MergeSort {
+public class MergeSortIterative {
     public static void main(String[] args) {
         int[] array = new int[] { 1, 15, 78, 89, 9, 12, 12546, 8, 879, 16, 54, 1 };
 

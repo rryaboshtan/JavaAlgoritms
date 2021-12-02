19fa14797f8ed4e10cbc530b1dd5977be34f220d
rryaboshtan <rrvworkroman@gmail.com>
2021-12-02 02:11:27 +0200

    merge Sorting

--- MergeSort.java
+++ MergeSort.java
@@ -1,18 +1,48 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 public class MergeSort {
     public static void main(String[] args) {
+        int[] array = new int[] { 1, 15, 78, 89, 9, 12, 12546, 8, 879, 16, 54, 1 };
 
+        System.out.println(arrayToString(array));
+        array = mergeSort(array);
+        System.out.println(arrayToString(array));
     }
 
-    public static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int dest[], int destStart, int size) {
+    public static int[] mergeSort(int[] array) {
+        int[] temp;
+        int[] currentSrc = array;
+        int[] currentDest = new int[array.length];
+
+        int size = 1;
+        while (size < array.length) {
+            for (int i = 0; i < array.length; i += 2 * size) {
+                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
+            }
+
+            temp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = temp;
+
+            size = size * 2;
+
+            System.out.println(arrayToString(currentSrc));
+        }
+        return currentSrc;
+    }
+
+    public static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int dest[], int destStart,
+            int size) {
         int index1 = src1Start;
         int index2 = src2Start;
 
         int src1End = Math.min(src1Start + size, src1.length);
         int src2End = Math.min(src2Start + size, src2.length);
+
+        // if (src1Start + size > src1.length) {
+        //     for (int i = src1Start; i < src1End; i++) {
+        //         dest[i] = src1[i];
+        //     }
+        //     return;
+        // }
 
         int iterationCount = src1End - src1Start + src2End - src2Start;
 
@@ -25,5 +55,18 @@ public class MergeSort {
                 index2++;
             }
         }
+    }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
